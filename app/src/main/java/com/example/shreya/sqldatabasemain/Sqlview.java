0720197bed863d254adeaba5f4d77c2747995fc9
Shryaa <shreya.wadhwa321@gmail.com>
2017-07-29 15:29:52 -0700

    database

---
+++ app/src/main/java/com/example/shreya/sqldatabasemain/Sqlview.java
@@ -0,0 +1,21 @@
+package com.example.shreya.sqldatabasemain;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by Shryaa on 7/25/2017.
+ */
+
+public class Sqlview extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sqlview);
+
+        
+    }
+}
